0cbb3ba48a847846407bc5815690525b68230e92
Puentebravo <puentematos@gmail.com>
2021-09-11 00:06:57 -0400

    separated service worker registration script into different file

---
+++ assets/swregister.js
@@ -0,0 +1,11 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker Online.");
+      })
+      .catch((error) => {
+        console.log("Service Worker halted. Tracing error.");
+        console.log(error);
+      });
+  }
