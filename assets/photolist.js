813a591fadb234243dd1e2c8a62a941b9acfcdb2
Puentebravo <puentematos@gmail.com>
2022-03-14 19:37:03 -0400

    updated sizing on photos for better performance

---
+++ assets/photolist.js
@@ -0,0 +1,30 @@
+ const photoArray = [
+  {
+    source: "./assets/images/kidflorian.jpg",
+    alt: "Floyd in his native brooklyn",
+    ariaLabel: "Slide 1",
+  },
+  {
+    source: "./assets/images/youngFloyd.jpg",
+    alt: "Floyd in his Youth",
+    ariaLabel: "Slide 2",
+  },
+  {
+    source: "./assets/images/johnMatos.jpg",
+    alt: "John Matos",
+    ariaLabel: "Slide 3",
+  },
+  {
+    source: "./assets/images/Dolores.jpg",
+    alt: "The future Mrs. Matos",
+    ariaLabel: "Slide 4",
+  },
+  {
+    source: "./assets/images/manofthesea.jpg",
+    alt: "The old man of the sea",
+    ariaLabel: "Slide 5",
+  },
+];
+
+
+export default photoArray
